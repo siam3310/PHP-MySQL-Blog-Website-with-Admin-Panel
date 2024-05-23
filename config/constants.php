67f25d6731c4f76cc91734e01efc206db69866f9
siam3310 <164713293+siam3310@users.noreply.github.com>
2024-05-23 17:33:38 +0600

    Update constants.php

--- config/constants.php
+++ config/constants.php
@@ -1,8 +1,8 @@
 <?php
 session_start();
-define("ROOT_URL", "http://localhost/blog/");
-define('DB_HOST', 'localhost');
-define('DB_USER', 'underemployed');
-define('DB_PASS', 'admin1234');
-define('DB_NAME', 'blog');
+define("ROOT_URL", "/");
+define('DB_HOST', 'sql307.infinityfree.com');
+define('DB_USER', 'if0_36599985');
+define('DB_PASS', 'FGV5lJcZnBD');
+define('DB_NAME', 'if0_36599985_cinebuzz');
 
